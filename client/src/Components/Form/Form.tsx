e5f9c60fa4253eeced10e7e1988223b5ef0b50c3
Bearcin46 <imsweety33@gmail.com>
2024-05-17 20:03:56 +0530

    chore: input work going on

--- client/src/Components/Form/Form.tsx
+++ client/src/Components/Form/Form.tsx
@@ -1,10 +1,39 @@
 import FormInput from "./FormInput";
+import { useForm } from "react-hook-form";
+
+interface formDatas {
+  fullName: string;
+  profileUrl: string;
+  backgroundUrl: string;
+  bio: string;
+}
 
 const Form = () => {
+  const {
+    formState: { errors },
+    register,
+  } = useForm<formDatas>();
   return (
     <div>
       <form action="">
-        <FormInput type={"text"} name={"Enter your first name"} />
+        {/* full name */}
+        <FormInput
+          type={"text"}
+          label={"Full Name"}
+          name={"fullName"}
+          placeholder={"Enter your fullname"}
+          error={errors.fullName}
+          register={register("fullName")}
+        />
+        {/* Profile url */}
+        <FormInput
+          type={"text"}
+          label={"Profile Url"}
+          name={"profilrUrl"}
+          placeholder={"Profile picture link"}
+          error={errors.fullName}
+          register={register("profileUrl")}
+        />
       </form>
     </div>
   );
